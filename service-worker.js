82bece0e93918b020a162314af47cdfab14e20ea
Sergio Kagiema- Fay <sergio_kagiema@live.com>
2018-06-04 22:31:07 +0300

    Updated sw.js

--- service-worker.js
+++ service-worker.js
@@ -21,34 +21,23 @@ const cacheFiles = [
 ];
 
 self.addEventListener('install', function(e) {
-  
   console.log('[Service Worker installed]')
-
   e.waitUntil(
     caches.open(cacheName).then( cache => cache.addAll(cacheFiles))
   )
-
 });
 
-self.addEventListener('activate', function(e) {
-  console.log('[Service Worker activated]')
+self.addEventListener('fetch', function(e) {
+  console.log('[Service Worker fetching]');
+  return e.respondWith(
+    caches.match(
+      e.request, 
+      // ignoring the query string in the URL.
+      { ignoreSearch: true }
+    )
+    .then(response => {
+      return response || fetch( e.request );
+    })
+    .catch(err => console.log( err, e.request ))
+  );
 });
-
-self.addEventListener('fetch', event => {
-    // Let the browser do its default thing
-    // for non-GET requests.
-    if (event.request.method != 'GET') return;
-
-    event.respondWith(
-        caches.open('restaurants_v1').then(function(cache) {
-            return cache.match(event.request).then(function (response) {
-            return response || fetch(event.request).then(function(response) {
-                cache.put(event.request, response.clone());
-                return response;
-            }).catch(e => console.error(e));
-            });
-        })
-    );
-
-
-  });
